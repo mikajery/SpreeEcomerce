28f33f1bb9276ec14e9c26df655fa9a4a1be3024
Administrator <example@mail.com>
2014-05-08 09:32:55 +0300

    modify some files

--- app/overrides/custom-admin-tab.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-                     :virtual_path => 'spree/layouts/admin',
-                     :name => 'custom-admin-tab',
-                     :replace => "nav#admin-menu",
-                     :text => '<h1>This is the new NAV</h1>'
-                    )
