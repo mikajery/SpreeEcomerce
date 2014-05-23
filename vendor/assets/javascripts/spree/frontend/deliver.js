e8a9c65e75bd9e211d169df0d88f461625125d8c
admin <admin@mail.com>
2014-05-23 09:43:29 -0400

    complet delivery module

--- vendor/assets/javascripts/spree/frontend/deliver.js
+++ vendor/assets/javascripts/spree/frontend/deliver.js
@@ -3,5 +3,6 @@ $(function(){
 	$('#deli-form').css("display", "none");
 	$("#add-delivery").click(function(){
 		$('#deli-form').css("display", "block");
+		$(this).css("display", "none");
 	});
 })
